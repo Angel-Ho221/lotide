437f3743651ca57a6b3b137827db16b97738b708
Angel-Ho221 <angel.ho@ryerson.ca>
2020-06-22 23:47:19 +0000

    Implenement tail

---
+++ tail.js
@@ -0,0 +1,10 @@
+function assertEqual(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+function tail(array) {
+  return array.slice(1);
+}
