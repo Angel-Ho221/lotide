ff379fac48a4a7e087f00decc3939c5e15b3cfe9
Angel-Ho221 <angel.ho@ryerson.ca>
2020-06-22 23:28:50 +0000

    Implementing Head

---
+++ head.js
@@ -0,0 +1,13 @@
+function assertEqual(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+function head(array, value) {
+  return array[0];
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
