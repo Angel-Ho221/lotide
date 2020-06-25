378b3391477208082d741e896076650bcd13de78
Angel-Ho221 <angel.ho@ryerson.ca>
2020-06-25 17:50:26 +0000

    countOnly lotide

---
+++ countOnly.js
@@ -0,0 +1,37 @@
+function assertEqual(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+function countOnly(allItems, itemsToCount) {
+let result = {}
+for (let item of allItems) {
+  if (itemsToCount[item]) {
+      if (result[item]) {
+      result[item] ++
+    } else {
+      result[item] = 1
+    }
+  } 
+}
+  return result;
+}
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
