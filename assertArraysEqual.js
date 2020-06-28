94624e9a6804559b5dbfeb321bffc23fb11b155e
Angel-Ho221 <angel.ho@ryerson.ca>
2020-06-28 22:32:49 +0000

    opps forgot to add this on git - combined assert equal and eqArrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,25 @@
+
+function assertArraysEqual(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+
+function eqArrays(arrayOne, arrayTwo) {
+  // test to see if arrayOne equals to arrayTwo
+  let equal = true
+  if (arrayOne.length !== arrayTwo.length) {
+    equal = false 
+    return equal;
+  } 
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] === arrayTwo[i]) {
+      equal = true
+    } else {
+      equal = false
+    }
+  }
+  return equal;
+}
