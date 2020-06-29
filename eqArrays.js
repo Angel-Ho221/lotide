a5411d1156e7824272213248322b4acbbc7da87b
Angel-Ho221 <angel.ho@ryerson.ca>
2020-06-29 18:56:24 +0000

    refactored eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,5 @@
-function assertEqual(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-}
+const assertEqual = require('./assertEqual');
+
 // function eqArrays takes in two arrays and returns true or false based on a perfect match
 function eqArrays(arrayOne, arrayTwo) {
   // test to see if arrayOne equals to arrayTwo
@@ -12,7 +7,7 @@ function eqArrays(arrayOne, arrayTwo) {
   if (arrayOne.length !== arrayTwo.length) {
     equal = false 
     return equal;
-  }
+  } 
   for (let i = 0; i < arrayOne.length; i++) {
     if (arrayOne[i] === arrayTwo[i]) {
       equal = true
@@ -22,4 +17,4 @@ function eqArrays(arrayOne, arrayTwo) {
   }
   return equal;
 }
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays; 
